a216b38579f7c06d4b414725e187d0a236ac2b93
Dileep <dileep.ks75@gmail.com>
2014-11-20 11:34:38 +0530

    My Working code commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## 1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## 2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        set <- function( matrix ) {
+                x <<- matrix
+                inv <<- NULL
+        }
+        
+        get <- function() {
+                ## Return the matrix
+                x
+        }
+        
+        setInverse <- function(inverse) {
+                inv <<- inverse
+        }
+        
+        getInverse <- function() {
+                ## Return the inverse property
+                inv
+        }
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
+        
 }
 
 
@@ -12,4 +40,39 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getInverse()
+        if( !is.null(m) ) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
 }
+
+
+## Sample Run result as follows.
+##> source("cachematrix.R")
+##> a<-makeCacheMatrix(matrix(c(1,2,11,12),2,2))
+##> summary(a)
+##Length Class  Mode    
+##set        1      -none- function
+##get        1      -none- function
+##setInverse 1      -none- function
+##getInverse 1      -none- function
+##> a$get()
+##[,1] [,2]
+##[1,]    1   11
+##[2,]    2   12
+##> cacheSolve(a)
+##[,1] [,2]
+##[1,] -1.2  1.1
+##[2,]  0.2 -0.1
+##> cacheSolve(a)
+##getting cached data
+##[,1] [,2]
+##[1,] -1.2  1.1
+##[2,]  0.2 -0.1
+##> 
